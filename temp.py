539ed81bcbd694f073b81791a2880698eb28ab54
Kartik Sachdeva <kartik@Kartiks-MacBook-Air.local>
2018-09-19 21:22:14 +0530

    Updating python file

--- temp.py
+++ temp.py
@@ -8,18 +8,23 @@ Created on Wed Sep 19 19:48:50 2018
 
 import pandas as pd
 import os
-import glob
 
-
+#Setting directory
 path = os.getcwd()
 files = os.listdir(path)
 
-
+#Storing names of xls files in a list
 files_xls = [f for f in files if f[-3:] == 'xls']
 
+#creating an empty dataFrame
 df = pd.DataFrame
+
+#ls is the list to store the data of each xls file
+ls = []
 
 for f in files_xls:
     data = pd.read_excel(f, 'Sheet1')
-    df = df.append(data)
-    
+    ls.append(data)
+
+#Concatenate data from ls into data frame result
+result = pd.concat(ls)
